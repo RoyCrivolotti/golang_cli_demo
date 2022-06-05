24f6c4becc303e3bc101264669b283492db56943
rcrivolotti <roygabriel.crivolotti@mercadolibre.com>
2022-06-05 19:16:10 -0300

    Minor edits

--- notifier/src/notifier.go
+++ notifier/src/notifier.go
@@ -9,17 +9,6 @@ import (
 	"refurbedchallenge/notifier/constants"
 )
 
-//Library
-//Create HTTP notifications client -> Configurable URL to which to propagate notifications
-//Client gets message and sends them in the body of the POST request to the configured URL in a NON BLOCKER manner
-//Should the Client get an error when sending the message, the caller should be able to handle the error
-
-//Executable
-//Consume library above
-//Send new messages every interval (configurable)
-//The exe reads standard input and every new line is a message to send to the library for propagation
-//Consider shutdown by SIGINT
-
 //go:generate mockgen -source=./notifier.go -destination=./mock/notifier_mock.go
 
 type INotifierClient interface {
